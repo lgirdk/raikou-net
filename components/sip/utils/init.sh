d28688816be93c01379a223c895727fed2c1086b
Saurav Kochar <saurav.kochar01@infosys.com>
2024-07-09 14:31:29 +0200

    fix(sip): add isolate_docker_iface, add ENV and fix init.sh
    BOARDFARM-5080
    
    Change-Id: Id37cb95b6ea7760a443d3ec61f334f140255eaaa
    Signed-off-by: Saurav Kochar <saurav.kochar01@infosys.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/raikou-net/+/7624
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- components/sip/utils/init.sh
+++ components/sip/utils/init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+if [ "$LEGACY" == "no" ]; then
+    isolate_docker_iface
+fi
+
 #if no DNS servers are specifified, defaults will be used
 echo "nameserver ${DNS_IPv4:-"172.25.1.2"}" > /etc/resolv.conf
 echo "nameserver ${DNS_IPv6:-"2001:dead:beef:2::2"}" >> /etc/resolv.conf
-
-
 
 cat > /etc/kamailio/kamctlrc << EOF
 SIP_DOMAIN=sipcenter.boardfarm.com
@@ -28,8 +30,8 @@ rtpproxy -F -s udp6:localhost:7722
 
 sed -i "s/kam_listen_ipv4/udp:$KAM_LISTEN_IPv4:5060/" /etc/kamailio/kamailio.cfg
 sed -i "s/kam_listen_ipv6/udp:[$KAM_LISTEN_IPv6]:5060/" /etc/kamailio/kamailio.cfg
-sed -i "s/kam_listen_alias/udp:$KAM_LISTEN_ALIAS:5060/" /etc/kamailio/kamailio.cfg
-
-/usr/sbin/sshd -D
+sed -i "s/kam_listen_alias/$KAM_LISTEN_ALIAS/" /etc/kamailio/kamailio.cfg
 
 service kamailio start
+
+/usr/sbin/sshd -D
