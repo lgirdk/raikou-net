daa2633cf16b4b1713d01f0e299e9fd0313ffc6f
Saurav Kochar <saurav.kochar01@infosys.com>
2024-07-19 15:37:57 +0200

    feat(phone): add new dockerfile for phone
    latest version of pjsua
    
    BREAKING CHANGE: BOARDFARM-5080
    
    Change-Id: I713a9aa9fa820c9963d878138afbf415773af814
    Signed-off-by: Saurav Kochar <saurav.kochar01@infosys.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/raikou-net/+/7810
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

---
+++ components/phone/utils/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+if [ "$LEGACY" == "no" ]; then
+    isolate_docker_iface
+fi
+
+mv /root/pjproject/pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu /root/pjproject/pjsip-apps/bin/pjsua
+
+#if no DNS servers are specifified, defaults will be used
+echo "nameserver ${DNS_IPv4:-"172.25.1.2"}" > /etc/resolv.conf
+echo "nameserver ${DNS_IPv6:-"2001:dead:beef:2::2"}" >> /etc/resolv.conf
+
+
+echo "export PATH=/root/pjproject/pjsip-apps/bin:$PATH" >> /root/.bashrc
+
+/usr/sbin/sshd -D
